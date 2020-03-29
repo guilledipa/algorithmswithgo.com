87e1a923b9a559830481e4257a1fe37424e54273
guilledipa <guilledipasquale@gmail.com>
2020-03-29 21:10:41 +1100

    Another problem

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"fmt"
+	"log"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +14,16 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	var res int
+	mult := 1
+	for i := len(value) - 1; i >= 0; i-- {
+		var val int
+		_, err := fmt.Sscanf(string(value[i]), "%X", &val)
+		if err != nil {
+			log.Panicf("Unable to translate value: %v", value[i])
+		}
+		res += mult * val
+		mult *= base
+	}
+	return res
 }
